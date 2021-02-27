6b134ef968f820687e37dececd15b932d68b0250
Deep Prabhat Tirkey <68368368+S1MPLYFAiTH@users.noreply.github.com>
2021-02-27 21:10:30 +0530

    Update secrets.h

--- WebAppIno/secrets.h
+++ WebAppIno/secrets.h
@@ -1,9 +1,8 @@
-
-// Use this file to store all of the private credentials
+// Use this file to store all of the private credentials 
 // and connection details
 
-#define SECRET_SSID "E-1"        // replace MySSID with your WiFi network name
-#define SECRET_PASS "9437097000" // replace MyPassword with your WiFi password
+#define SECRET_SSID "MySSID"		// replace MySSID with your WiFi network name
+#define SECRET_PASS "MyPassword"	// replace MyPassword with your WiFi password
 
-#define SECRET_CH_ID 1313863                   // replace 0000000 with your channel number
-#define SECRET_WRITE_APIKEY "RR306UHPNWZ0SA22" // replace XYZ with your channel write API Key
+#define SECRET_CH_ID 000000			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "XYZ"   // replace XYZ with your channel write API Key
